9ddbb01a5a3d11a6692c92770f08a936c79d3e58
Gavin <gavin.a.gregory@gmail.com>
2022-04-18 17:23:45 -0700

    remove api keys

--- config/plugins.js
+++ config/plugins.js
@@ -3,9 +3,9 @@ module.exports = ({ env }) => ({
     config: {
       provider: "cloudinary",
       providerOptions: {
-        cloud_name: env("CLOUDINARY_NAME", "dcm7sy0wf"),
-        api_key: env("CLOUDINARY_KEY", "828171717283695"),
-        api_secret: env("CLOUDINARY_SECRET", "oUK8PTLvLk0cTZmtdijRRAlB_0w"),
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
       },
       actionOptions: {
         upload: {},
